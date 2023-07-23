7c36004c31ab1a7c8fb81530d30d1e8a8a1a6141
eukkki210 <ysg980423@gmail.com>
2023-07-24 02:05:40 +0900

    revise: floating buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,22 @@
-$(document).ready(function() {
-    $(".goods-items").children().clone().appendTo(".goods-items");
+// 페이지 상단으로 올라가는 함수
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
-    var totalWidth = 0;
-    $(".goods-items").children().each(function() {
-        totalWidth += $(this).outerWidth(true);
-    });
-
-    $(".goods-items").width(totalWidth);
-
-    function resetAnimation() {
-        $(".goods-items").css("animation", "none");
-        $(".goods-items").width();
-        $(".goods-items").css("animation", "leftMoveLoop 30s linear infinite");
+// 다크 모드 토글 함수
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+    const toggleButton = document.getElementById('toggle');
+    if (body.classList.contains('dark-mode')) {
+        toggleButton.textContent = "라이트 모드로 이용하기";
+    } else {
+        toggleButton.textContent = "다크 모드로 이용하기";
     }
+}
 
-    $(".goods-items").on("animationiteration", resetAnimation);
-});
+// 고정 플로팅 버튼 클릭 이벤트 리스너
+document.getElementById('goTopBtn').addEventListener("click", goToTop);
+
+// 다크 모드 토글 버튼 클릭 이벤트 리스너
+document.getElementById('toggle').addEventListener("click", toggleDarkMode);
